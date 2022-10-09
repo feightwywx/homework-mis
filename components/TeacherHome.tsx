cb8446b2e78816071cbff19bc9ef8d4561e6a828
feightwywx <kururinmiracle@outlook.com>
2022-10-09 10:51:50 +0800

    Fix: 加载动画位置偏移

--- components/TeacherHome.tsx
+++ components/TeacherHome.tsx
@@ -143,7 +143,6 @@ export function TeacherHome(): JSX.Element {
     })
   }
 
-  // FIXME: 展开项目无法折叠
   const [treeExpandedKeys, setTreeExpandedKeys] = useState<Key[]>([]);
 
   function onTreeChange(checkedValues: string[]) {
@@ -160,154 +159,157 @@ export function TeacherHome(): JSX.Element {
 
   return (
     <HwLayout>
-      <Spin spinning={!(user && homework)}>
-        {homework && <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-          <Title style={{ marginBottom: '16px' }}>{
-            hour < 5 ? '夜深了' : (
-              hour > 5 && hour < 9 ? '早上好' : (
-                hour < 12 ? '上午好' : (
-                  hour < 14 ? '中午好' : (
-                    hour < 18 ? '下午好' : (
-                      hour < 22 ? '晚上好' : '夜深了'
-                    )
+      {!(user && homework) &&
+        <div style={{ margin: '32px', textAlign: 'center' }}>
+          <Spin></Spin>
+        </div>
+      }
+      {homework && <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+        <Title style={{ marginBottom: '16px' }}>{
+          hour < 5 ? '夜深了' : (
+            hour > 5 && hour < 9 ? '早上好' : (
+              hour < 12 ? '上午好' : (
+                hour < 14 ? '中午好' : (
+                  hour < 18 ? '下午好' : (
+                    hour < 22 ? '晚上好' : '夜深了'
                   )
                 )
               )
             )
+          )
 
-          }，{user?.name}</Title>
-          <Title level={4} style={{ marginTop: 0 }}>{(() => {
+        }，{user?.name}</Title>
+        <Title level={4} style={{ marginTop: 0 }}>{(() => {
+          const filtered = homework?.filter(
+            item => currtime <= parseMysqlDateTime(item.deadline)
+          );
+          if (filtered
+            && filtered.length !== 0
+          ) {
+            return `目前已经下发了${filtered.length}项作业。`;
+          } else {
+            return '目前没有正在进行的作业。';
+          }
+        })()}</Title>
+        <Divider />
+        <div>
+          <Button
+            type='primary'
+            shape='round'
+            icon={<FormOutlined />}
+            style={{ float: 'right' }}
+            onClick={() => { setModalOpen(true); }}>下发作业</Button>
+          <Title level={5} style={{ float: 'left' }}>进行中作业</Title>
+        </div>
+        <Row gutter={16}>
+          {(() => {
             const filtered = homework?.filter(
               item => currtime <= parseMysqlDateTime(item.deadline)
             );
-            if (filtered
-              && filtered.length !== 0
-            ) {
-              return `目前已经下发了${filtered.length}项作业。`;
-            } else {
-              return '目前没有正在进行的作业。';
-            }
-          })()}</Title>
-          <Divider />
-          <div>
-            <Button
-              type='primary'
-              shape='round'
-              icon={<FormOutlined />}
-              style={{ float: 'right' }}
-              onClick={() => { setModalOpen(true); }}>下发作业</Button>
-            <Title level={5} style={{ float: 'left' }}>进行中作业</Title>
-          </div>
-          <Row gutter={16}>
-            {(() => {
-              const filtered = homework?.filter(
-                item => currtime <= parseMysqlDateTime(item.deadline)
-              );
-              return filtered?.length ? (
-                filtered.map((item, index) => (
-                  <Col xs={24} md={12} xl={8} key={index}>
-                    <HomeworkCard homework={item} />
-                  </Col>
-                ))
-              ) : (
-                <Col span={24} style={{ marginBottom: 12 }}>
-                  <Text>暂时还没有进行中作业～</Text>
+            return filtered?.length ? (
+              filtered.map((item, index) => (
+                <Col xs={24} md={12} xl={8} key={index}>
+                  <HomeworkCard homework={item} />
                 </Col>
-              );
-            })()}
-          </Row>
-          <Title level={5}>已过期作业</Title>
-          <Row gutter={16}>
-            {(() => {
-              const filtered = homework?.filter(
-                item => currtime > parseMysqlDateTime(item.deadline)
-              );
-              return filtered?.length ? (
-                filtered.map((item, index) => (
-                  <Col xs={24} md={12} xl={8} key={index}>
-                    <HomeworkCard homework={item} />
-                  </Col>
-                ))
-              ) : (
-                <Col span={24} style={{ marginBottom: 12 }}>
-                  <Text>暂时还没有已过期作业～</Text>
+              ))
+            ) : (
+              <Col span={24} style={{ marginBottom: 12 }}>
+                <Text>暂时还没有进行中作业～</Text>
+              </Col>
+            );
+          })()}
+        </Row>
+        <Title level={5}>已过期作业</Title>
+        <Row gutter={16}>
+          {(() => {
+            const filtered = homework?.filter(
+              item => currtime > parseMysqlDateTime(item.deadline)
+            );
+            return filtered?.length ? (
+              filtered.map((item, index) => (
+                <Col xs={24} md={12} xl={8} key={index}>
+                  <HomeworkCard homework={item} />
                 </Col>
-              );
-            })()}
-          </Row>
-        </Space>
-        }
-        <Modal
-          open={modalOpen}
-          title='作业布置'
-          onCancel={() => { setModalOpen(false); }}
-          width={'80vw'}
-          style={{ minHeight: '80vh' }}
-          confirmLoading={confirmLoading}
-          onOk={() => {
-            form
-              .validateFields()
-              .then(values => {
-                assignClickHandler(values);
-              })
-              .catch(() => {
-              });
-          }}
-        >
-          <Form
-            form={form}
-            autoComplete='off'
-            requiredMark='optional'>
-            <Form.Item
-              label='作业标题'
-              name='title'
-              rules={[{
-                required: true,
-              }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label='作业描述'
-              name='assignment'
-              rules={[{
-                required: true,
-              }]}>
-              <TextArea
-                rows={10}
-                showCount
-              />
-            </Form.Item>
-            <Form.Item
-              label='截止日期'
-              name='deadline'
-              rules={[{
-                required: true,
-              }]}>
-              <DatePicker
-                format="YYYY-MM-DD HH:mm:ss"
-                showTime={{ defaultValue: moment('00:00:00', 'HH:mm:ss') }}
-                disabledDate={disabledDate}
-              />
-            </Form.Item>
-            <Form.Item
-              label='布置对象'
-              name='target'
-              rules={[{
-                required: true,
-              }]}>
-              <TreeSelect
-                treeDataSimpleMode
-                treeData={treeData}
-                treeCheckable
-                loadData={onLoadData}
-                treeExpandAction='click'
-                onChange={onTreeChange}
-                treeExpandedKeys={treeExpandedKeys}
-                onTreeExpand={onTreeExpand}
-              />
-            </Form.Item>
-          </Form>
-        </Modal>
-      </Spin>
+              ))
+            ) : (
+              <Col span={24} style={{ marginBottom: 12 }}>
+                <Text>暂时还没有已过期作业～</Text>
+              </Col>
+            );
+          })()}
+        </Row>
+      </Space>
+      }
+      <Modal
+        open={modalOpen}
+        title='作业布置'
+        onCancel={() => { setModalOpen(false); }}
+        width={'80vw'}
+        style={{ minHeight: '80vh' }}
+        confirmLoading={confirmLoading}
+        onOk={() => {
+          form
+            .validateFields()
+            .then(values => {
+              assignClickHandler(values);
+            })
+            .catch(() => {
+            });
+        }}
+      >
+        <Form
+          form={form}
+          autoComplete='off'
+          requiredMark='optional'>
+          <Form.Item
+            label='作业标题'
+            name='title'
+            rules={[{
+              required: true,
+            }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label='作业描述'
+            name='assignment'
+            rules={[{
+              required: true,
+            }]}>
+            <TextArea
+              rows={10}
+              showCount
+            />
+          </Form.Item>
+          <Form.Item
+            label='截止日期'
+            name='deadline'
+            rules={[{
+              required: true,
+            }]}>
+            <DatePicker
+              format="YYYY-MM-DD HH:mm:ss"
+              showTime={{ defaultValue: moment('00:00:00', 'HH:mm:ss') }}
+              disabledDate={disabledDate}
+            />
+          </Form.Item>
+          <Form.Item
+            label='布置对象'
+            name='target'
+            rules={[{
+              required: true,
+            }]}>
+            <TreeSelect
+              treeDataSimpleMode
+              treeData={treeData}
+              treeCheckable
+              loadData={onLoadData}
+              treeExpandAction='click'
+              onChange={onTreeChange}
+              treeExpandedKeys={treeExpandedKeys}
+              onTreeExpand={onTreeExpand}
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
     </HwLayout>);
 }
