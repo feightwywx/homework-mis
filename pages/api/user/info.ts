485b3dad7271d9cf45d4c0b8492d7c73c733f36d
feightwywx <kururinmiracle@outlook.com>
2023-04-28 01:42:56 +0800

    Fix: make eslint happy

--- pages/api/user/info.ts
+++ pages/api/user/info.ts
@@ -1,38 +1,45 @@
-import { withIronSessionApiRoute } from 'iron-session/next'
-import { sessionOptions } from '../../../utils/session'
-import { NextApiRequest, NextApiResponse } from 'next'
-import { getNameByToken } from '../../../utils/user'
-import { JsonResponse, User } from '../../../utils/types'
-import { successResponse } from '../../../utils/api'
+import { withIronSessionApiRoute } from "iron-session/next";
+import { sessionOptions } from "../../../utils/session";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getNameByToken } from "../../../utils/user";
+import { JsonResponse, User } from "../../../utils/types";
+import { successResponse } from "../../../utils/api";
 
-async function userRoute(req: NextApiRequest, res: NextApiResponse<JsonResponse<User>>) {
+async function userRoute(
+  req: NextApiRequest,
+  res: NextApiResponse<JsonResponse<User>>
+) {
   if (req.session.user) {
-    const user = req.session.user
-    const name = await getNameByToken(user.userType, user.token)
+    const user = req.session.user;
+    const name = await getNameByToken(user.userType, user.token);
     const resJson = {
       ...user,
       isLoggedIn: true,
-    }
+    };
 
-
-    if (name !== null) {    // 如果获取到name，则写入到返回体中
+    if (name !== null) {
+      // 如果获取到name，则写入到返回体中
       req.session.user = { ...user, name };
       await req.session.save();
-      res.json(successResponse({
-        ...resJson,
-        name
-      }))
+      res.json(
+        successResponse({
+          ...resJson,
+          name,
+        })
+      );
     } else {
       res.json(successResponse(resJson));
     }
-
   } else {
-    res.json(successResponse({
-      isLoggedIn: false,
-      name: '',
-      userType: 'student',
-      token: ''
-    }))
+    res.json(
+      successResponse({
+        isLoggedIn: false,
+        name: "",
+        userType: "student",
+        token: "",
+        actual_id: "",
+      })
+    );
   }
 }
 
